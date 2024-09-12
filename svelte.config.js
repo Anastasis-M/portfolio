abc7c0b80adc30f9f8f18efdc982b6284d1b532e
Anastasis-M <77619714+Anastasis-M@users.noreply.github.com>
2024-09-12 17:20:43 +0300

    base parameter for domain change

--- svelte.config.js
+++ svelte.config.js
@@ -1,7 +1,6 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
-const base = '/portfolio';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
