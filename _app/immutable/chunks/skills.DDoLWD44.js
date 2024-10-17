893426c63f99fbaa489603fdb9b000665a3e902b
Anastasis-M <77619714+Anastasis-M@users.noreply.github.com>
2024-10-17 08:05:22 +0000

    Deploying to gh-pages from @ Anastasis-M/portfolio@93ae3b7986132ff401d14ffa8c8270a16b88076f 🚀

--- _app/immutable/chunks/skills.DDoLWD44.js
+++ _app/immutable/chunks/skills.DDoLWD44.js
@@ -1,4 +1,4 @@
-import{A as n}from"./UIcon.CWQB6pXf.js";const m=`# Svelte
+import{A as n}from"./UIcon.CIU8OOPj.js";const m=`# Svelte
 
 ---
 
--- _app/immutable/entry/start.CQFiZQYO.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/entry.Dr75WVgJ.js";export{t as start};
