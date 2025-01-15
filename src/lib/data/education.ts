fe6234c70ed17edcea6765621767a9d23a48c481
Anastasis-M <77619714+Anastasis-M@users.noreply.github.com>
2025-01-15 15:41:32 +0200

    chore (education): updated period adn description for bachelor degree

--- src/lib/data/education.ts
+++ src/lib/data/education.ts
@@ -4,12 +4,12 @@ import type { Education } from '../types';
 export const items: Array<Education> = [
 	{
 		degree: 'Bachelor of Informatics',
-		description: 'Gained foundational knowledge in computer science, software development, and information systems, with hands-on experience in programming, databases, and algorithms. Focused more on assignments about web development.',
+		description: 'Completed comprehensive studies in computer science, software development, and information systems, with hands-on experience in programming, databases, and algorithms. Special emphasis on web development projects.',
 		location: 'Piraeus, Greece',
 		logo: Assets.Unipi_logo, 
 		name: 'Bachelor\'s in Informatics',
 		organization: 'University of Piraeus',
-		period: { from: new Date('2020-10')},
+		period: { from: new Date('2020-10'), to: new Date('2024-09-30')},
 		shortDescription: '',
 		slug: 'informatics-bcs',
 		subjects: ['JavaScript', 'Svelte', 'Tailwind', 'HTML', 'CSS', 'Databases', 'Algorithms', 'Data Structures', 'Java', 'Python', 'C++', 'C#', 'Unity'] // Replace or add more relevant subjects
